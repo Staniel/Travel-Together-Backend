c5756ff309678d5b9d5419bc0ca2c32db1811929
LixinYao <yaolixinylx@gmail.com>
2015-04-12 11:09:57 -0400

    test config

---
+++ scripts/createadmin.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+
+if User.objects.count() == 0:
+    admin = User.objects.create_user('username', 'email@example.com', 'password')
+    admin.is_superuser = True
+    admin.is_staff = True
+    admin.save()
