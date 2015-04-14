82afd4441a2bd68adc90145fab407b23b15fdb89
LixinYao <yaolixinylx@gmail.com>
2015-04-13 22:24:47 -0400

    static

--- TravelTogether/settings.py
+++ TravelTogether/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 try:
  from local_settings import *
