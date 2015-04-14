c386706d5f5eeeb581f2b8769d70c7546a86cc08
LixinYao <yaolixinylx@gmail.com>
2015-04-13 22:19:09 -0400

    add static admin file

--- TravelTogether/settings.py
+++ TravelTogether/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%eyai-%&^*e989tobl(*nxd4)y7_k!#hzbir2o56^gh-$ajq$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 try:
  from local_settings import *
